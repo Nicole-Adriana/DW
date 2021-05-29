901f438155675d12826cd32bbba85a5d7006e85c
adriana <nicolebravohidalgo@gmail.com>
2021-05-29 09:00:04 -0500

    sesion17

--- 01_TrabajosDW_GB/Sesion16/Sesion-16.2/js/javascript.js
+++ 01_TrabajosDW_GB/Sesion16/Sesion-16.2/js/javascript.js
@@ -1,22 +1,32 @@
 
-function compara(){
-    var a=2;
-    var b=2;
-    if(a==b)
-    alert("a y b son iguales")
-    else
-    alert("a y b son diferentes")
-}
+// function compara(){
+//     var a=2;
+//     var b=2;
+//     if(a==b)
+//     alert("a y b son iguales")
+//     else
+//     alert("a y b son diferentes")
+// }
 
-function resta(x,y){
-    var rest=x-y;
-    document.write("<h2> La resta es" + rest + "</h2>");
-}
+// function resta(x,y){
+//     var rest=x-y;
+//     document.write("<h2> La resta es" + rest + "</h2>");
+// }
 
-function suma(a,b){
-    var sum=a+b;
-    document.getElementById("sumar").innerHTML="la suma es: " + sum;
-}
+// function suma(a,b){
+//     var sum=a+b;
+//     document.getElementById("sumar").innerHTML="la suma es: " + sum;
+// }
+// var funcion = prompt("¿Cuál es tu función nativa favorita?");
+
+// if(funcion)
+//     alert("Función introducida");
+//     else
+//     alert("No has introducido un usario");
+
+
+    confirm("¿Esta es una función?");
+
 
 
 
